a57d1c10f3d3bf8589f1cad97469a3ed1c6b3f78
Vladimir Valiullin <vvvaliullin@edu.hse.ru>
2025-12-19 15:59:11 +0300

    add requirments; add config; small fixes

--- src/core/config.py
+++ src/core/config.py
@@ -1,8 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn, RedisDsn
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    
-    class Config:
-        env_file = ".env" # Указываем путь к файлу
+    POSTGRES_DSN: PostgresDsn
+    REDIS_URL: RedisDsn
+    SELENIUM_HOST: str = "selenium"
+    SELENIUM_PORT: int = 4444
+
+    PROJECT_NAME: str = "Higher School of Parsing"
+    DEBUG: bool = True
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
